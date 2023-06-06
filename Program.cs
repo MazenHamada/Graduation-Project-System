9e64bbfee51414f3262bcf40260c159ef729101f
Mazen Hamada <mazen.hamada.erc@gmail.com>
2023-06-06 13:55:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graduation_project_system
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new StudentForm(studentName: "Tarek Mohamed", teamId: "10", teamLeader: "No"));
+            Application.Run(new LoginForm());
+        }
+    }
+}
